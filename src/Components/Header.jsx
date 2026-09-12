b1cedc38fb1975ea375d19e5b304de88001db91b
Fares Fouad <faresfouadhussein90@gmail.com>
2026-09-13 01:26:51 +0300

    typescript

--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="header">
-      <h1>Welcome to TicTacToe</h1>
-    </header>
-  );
-}
-
-export default Header;
--- src/Components/PlayButton.jsx
+++
@@ -1,9 +0,0 @@
-function PlayButton() {
-  return (
-    <button className="play-button">
-      Play
-    </button>
-  );
-}
-
-export default PlayButton;
